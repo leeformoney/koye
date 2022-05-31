1ccdc9bdf93a5ef20724df505984a93c4fafa4a7
kichen <84363330+leeformoney@users.noreply.github.com>
2022-05-31 21:42:38 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "e25758a5-6120-4b96-97ef-294cf89e0504",
                         "alterId": 0
                     }
                 ],
